3690fcd0bd1ae8cd5d9739788036c653fcf0d69d
Jonathan Badger <jonathanbadger@gmail.com>
2025-09-15 16:36:40 -0500

    fix cv2 error on bbox viz

--- damo/utils/visualize.py
+++ damo/utils/visualize.py
@@ -8,7 +8,7 @@ __all__ = ['vis']
 
 
 def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
-
+    img = img.copy()
     for i in range(len(boxes)):
         box = boxes[i]
         cls_id = int(cls_ids[i])
@@ -27,6 +27,7 @@ def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
         font = cv2.FONT_HERSHEY_SIMPLEX
 
         txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
+        
         cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
 
         txt_bk_color = (_COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
